84f34e28fbfd8ea062da47c86a6968a7457f9aca
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-11-21 22:54:06 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/migrate', function (){
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh');
+    \App\Models\Admin::create([
+        'name' => 'Admin',
+        'email' => 'admin@gmail.com',
+        'password' => \Illuminate\Support\Facades\Hash::make('password'),
+    ]);
+    return \Illuminate\Support\Facades\Artisan::output();
+});
