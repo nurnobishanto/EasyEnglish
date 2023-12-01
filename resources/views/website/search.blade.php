f5aaa2f99f733cb499263b93d3277c1496ce65b0
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-12-01 23:15:44 +0600

    update search

--- resources/views/website/search.blade.php
+++ resources/views/website/search.blade.php
@@ -8,7 +8,7 @@
                     <div class="col">
                         <div class="page-banner-content" data-aos="fade-right" data-aos-delay="50" data-aos-duration="500" data-aos-once="true">
                             <h2>Search : {{request()->query('search')}}</h2>
-        
+
                             <ul>
                                 <li><a href="{{Route('website')}}">Home</a></li>
                                 {{-- <li><a href="{{Route('website.tagclouds')}}">Tags</a></li>
@@ -17,12 +17,12 @@
                         </div>
                     </div>
 
-               
+
                 </div>
             </div>
         </div>
         <!-- End Page Banner Area -->
-        
+
         <!-- Start Blog Area -->
         <div class="blog-area ptb-100">
             <div class="container">
@@ -36,22 +36,22 @@
                              <div class="col-lg-6 col-md-6">
                                 <div class="single-blog-card">
                                     <div class="blog-image">
-                                    
-                                        @if (Voyager::image( $post->image ))
-                                            <a href="{{Route('website.post',['slug'=>$post->slug])}}"><img src="{{ Voyager::image( $post->image )}}" alt="{{$post->title}}"></a>
+
+                                        @if ($post->image )
+                                            <a href="{{Route('website.post',['slug'=>$post->slug])}}"><img src="{{ asset('uploads/'. $post->image )}}" alt="{{$post->title}}"></a>
                                         @else
                                         <a href="{{Route('website.post',['slug'=>$post->slug])}}"><img src="{{ asset('website') }}/assets/images/blog/blog-1.jpg" alt="{{$post->title}}"></a>
                                         @endif
-                                
-                                        
+
+
                                     </div>
                                     <div class="blog-content with-padding">
                                         @if ($post->user)
                                             <span>By <a href="{{Route('website.author',['slug'=>$post->user->id])}}">{{$post->user->name}}</a></span>
                                         @else
-                                            
+
                                         @endif
-                                        
+
                                         <h3>
                                             <a href="{{Route('website.post',['slug'=>$post->slug])}}">{{$post->title}}</a>
                                         </h3>
@@ -60,14 +60,14 @@
                                         @else
                                             <p>{{ Str::limit( strip_tags($post->body), 100, ' ...');}}</p>
                                         @endif
-                                       
-                                        
-                                    
+
+
+
                                         <ul class="entry-meta">
                                             <li><i class="ri-calendar-line"></i> {{$post->created_at->format('M d, Y')}}</li>
                                             @if ($post->category)
-                                                <li><i class="ri-price-tag-3-line"></i> <a href="{{Route('website.category',['slug'=>$post->category->slug])}}">{{$post->category->name}}</a></li>      
-                                            @endif  
+                                                <li><i class="ri-price-tag-3-line"></i> <a href="{{Route('website.category',['slug'=>$post->category->slug])}}">{{$post->category->name}}</a></li>
+                                            @endif
                                             <li>
                                                 <i class="ri-eye-line"></i>
                                                 @if ($post->view_count<1000)
@@ -82,19 +82,19 @@
                                                         @php
                                                             $countm = $count/1000;
                                                           @endphp
-                                                         {{round($countm, 2)}}M     
+                                                         {{round($countm, 2)}}M
                                                       @endif
-                                                    
+
                                                 @endif
-                                                
+
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
-                              
+
                             @endforeach
-                         @endif 
+                         @endif
                         </div>
 
                         {{ $searchpost->appends(['search'=>request()->query('search')])->links('vendor.pagination.custom') }}
