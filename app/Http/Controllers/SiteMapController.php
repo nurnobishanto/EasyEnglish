483da2d8c64f2644dff6e165c06b2a537354b607
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-12-02 11:21:34 +0600

    update

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -12,16 +12,16 @@ class SiteMapController extends Controller
         $posts = Post::orderBy('updated_at', 'desc')->get();
         $subjects = Subject::orderBy('updated_at', 'desc')->get();
         $sitemap = resolve("sitemap");
-        $sitemap->add(route('website'), '2012-08-26T12:30:00+02:00', '1.0','daily');
-        $sitemap->add(route('website.contact'), '2012-08-26T12:30:00+02:00', '0.99','daily');
-        $sitemap->add(route('website.about'), '2012-08-26T12:30:00+02:00', '0.98','daily');
-        $sitemap->add(route('blog'), '2012-08-26T12:30:00+02:00', '0.95','daily');
-        $sitemap->add(route('exam'), '2012-08-26T12:30:00+02:00', '0.97','daily');
-        $sitemap->add(route('ebook'), '2012-08-26T12:30:00+02:00', '0.96','daily');
-        $sitemap->add(route('notes'), '2012-08-26T12:30:00+02:00', '0.94','daily');
-        $sitemap->add(route('website.category_clouds'), '2012-08-26T12:30:00+02:00', '0.93','daily');
-        $sitemap->add(route('login'), '2012-08-25T20:10:00+02:00', '0.8', 'monthly');
-        $sitemap->add(route('register'), '2012-08-25T20:10:00+02:00', '0.8', 'monthly');
+        $sitemap->add(route('website'), now(), '1.0','daily');
+        $sitemap->add(route('website.contact'), now(), '0.99','daily');
+        $sitemap->add(route('website.about'), now(), '0.98','daily');
+        $sitemap->add(route('blog'), now(), '0.95','daily');
+        $sitemap->add(route('exam'), now(), '0.97','daily');
+        $sitemap->add(route('ebook'), now(), '0.96','daily');
+        $sitemap->add(route('notes'), now(), '0.94','daily');
+        $sitemap->add(route('website.category_clouds'), now(), '0.93','daily');
+        $sitemap->add(route('login'), now(), '0.8', 'monthly');
+        $sitemap->add(route('register'), now(), '0.8', 'monthly');
 
 
         foreach ($posts as $post) {
